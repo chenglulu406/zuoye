a954cf7071a40554f09f76cff132ba7c2fed74d6
chenglulu406 <chenlulu406@163.com>
2013-05-09 23:53:41 +0900

    Update 求面积.py

--- 求面积.py
+++ 求面积.py
@@ -1,31 +1,39 @@
-# -*- coding: utf-8 -*-
-
-
-for i in range(1,2):
-    a=input("输入矩形边长：")
-    b=input("输入矩形边长：")
-    q=a*b
-    print "矩形面积："
-    print q
-    c=input("输入底:")
-    d=input("输入高:")
-    w=c*d/2
-    print"三角形面积："
-    print w
-    r=input("输入圆半径: ")
-    e=3.14 * (r ** 2)
-    print"圆面积："
-    print e
-    f=input("输入椭圆短半轴: ")
-    h=input("输入椭圆长半轴: ")
-    j=3.14*f*h
-    print"椭圆面积："
-    print j
-    s=q+w+e
-    print "总面积:"
-    print int(s)
-    print"平均面积："
-    m=s/4
-    print float(m)
-
-
+import math
+def jx():
+    width = input('矩形宽=？')
+    length = input("矩形长=？")
+    area2=width * length
+    return area2
+    def zjsjx():
+    width = input('直角三角形直角边1=？')
+    length = input("直角三角形直角边2=？")
+    area4=width * length / 2.0
+    return area4
+    def yx():    
+    r = input("圆的半径=？")
+    area1 = 3.14 * r ** 2
+    return area1
+    def pxsbx():
+    width = input('平行四边形底=？')
+    length = input("平行四边形高=？")
+    area3=width * length
+    return area3
+    def dbsjx():
+    a = input('等边三角形边长=？')
+    from math import sqrt
+    b = sqrt((a*a)-(a/2)**2)
+    area5=0.5 * a * b
+    return area5
+    def ty():
+    a = input('椭圆半长轴=？')
+    b = input("椭圆半短轴=？")
+    area6=math.pi*a*b
+    return are                                                                                                               
+    print '图形列表[1.圆,2.矩形(正方形)，3.平行四边形，4.直角三角形，5.等边三角形,6.椭圆]'                                                                                                                    
+    print '请依照列表输入各图形尺寸'
+    m=jx()+zjsjx()+yx()+px                                                                                                                    
+    sbx()+dbsjx()+ty()                                                                                                                    
+    print '总面积%s' %(m)                                                                                                                     
+    print '平均面积%s'%(m/6)                                                                                                                   
+                                                                                                                      
+                                                                                                                        
